dbc4f0e7f1301999153a06019b959d0ad5045497
Ivan Fomenko <Fomka1921@gmail.com>
2020-03-05 18:56:22 +0200

    revision v1.0

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Мобильное меню//
+function openNav() {
+    document.getElementById("nav-mobile").style.width = "100%";
+}
+  
+  function closeNav() {
+    document.getElementById("nav-mobile").style.width = "0%";
+}
+
+// Sing Up form//
+function openRegister() {
+  document.getElementById("sing-up-modal").style.width = "100%";
+}
+
+function closeSingUp() {
+  document.getElementById("sing-up-modal").style.width = "0%";
+}  
